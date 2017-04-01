e0b0d39865115c358a204cae1684616b1cdbb733
kelvinsun(孙承祚) <kelvinsun@tencent.com>
2017-04-01 23:36:56 +0800

    wirte file

---
+++ main.py
@@ -0,0 +1,44 @@
+__author__ = 'kelvinsun'
+import unittest
+import os
+import codecs
+import lxml
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+class seleniumTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.PhantomJS("D:\Program_Coding\phantomjs\\bin\phantomjs")
+
+    def testEle(self):
+        if not os.path.exists('./output'):
+            os.makedirs('./output')
+        self.rooms = codecs.open("./output/douyu.txt", "w", "utf-8")
+        page = 0
+        driver = self.driver
+        driver.get('http://www.douyu.com/directory/all')
+        soup = BeautifulSoup(driver.page_source, 'xml')
+        while True:
+            page += 1
+            print('parsing page:', page)
+            titles = soup.find_all('h3', {'class': 'ellipsis'})
+            nums = soup.find_all('span', {'class': 'dy-num fr'})
+            rooms = []
+            for title, num in zip(titles, nums):
+                rooms.append(title.get_text().strip() + "\t" + num.get_text().strip())
+            self.rooms.writelines([line + "\r\n" for line in rooms])
+            if driver.page_source.find('shark-pager-disable-next') != -1:
+                break
+            elem = driver.find_element_by_class_name('shark-pager-next')
+            elem.click()
+            soup = BeautifulSoup(driver.page_source, 'xml')
+
+    def tearDown(self):
+        # for item in self.rooms:
+            # print(item['title'], item['text'])
+        self.rooms.close()
+        print('down')
+
+if __name__ == "__main__":
+    unittest.main()
+
